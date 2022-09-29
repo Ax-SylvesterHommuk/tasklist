c9a1720a0e1757d3b67adb5efdf32252f7c48316
ax-sylvester <ax-sylvester.hommuk@khk.ee>
2022-09-29 11:13:46 +0300

    Clicking X button deletes the task[Delivers #183402494]

--- app.js
+++ app.js
@@ -1,22 +1,31 @@
-const form = document.querySelector("form")
-const taskInput = document.querySelector("#task")
+const form  = document.querySelector('form')
+const taskInput = document.querySelector('#task')
+const taskList = document.querySelector("ul")
 
-form.addEventListener("submit", addTask)
+form.addEventListener('submit', addTask)
+taskList.addEventListener('click', deleteTask)
 
-function addTask(event) {
-    console.log(taskInput.value)
-    const li = document.createElement("li")
+function addTask(e){
+    // create list item
+    const li = document.createElement('li')
     li.appendChild(document.createTextNode(taskInput.value))
-    li.className = "collection-item"
-    const a = document.createElement("a")
-    a.className = "blue-text text-darken 2 secondary-content"
-    a.setAttribute("href", "#")
+    li.className = 'collection-item'
+    const a = document.createElement('a')
+    a.appendChild(document.createTextNode('X'))
+    a.className = 'blue-text text-darken-2 secondary-content'
+    a.setAttribute('href', '#')
     li.appendChild(a)
-    // Add to list
-    const ul = document.querySelector("ul")
+    // add to list
+    const ul = document.querySelector('ul')
     ul.appendChild(li)
-    console.log(li)
-    event.preventDefault()
+    taskInput.value = ''
+    e.preventDefault()
 }
 
-
+function deleteTask(event){
+    if (event.target.textContent == "X"){
+        if (confirm("Are you sure you want to delete this task?")){
+            event.target.parentElement.remove()
+        }
+    }
+}
